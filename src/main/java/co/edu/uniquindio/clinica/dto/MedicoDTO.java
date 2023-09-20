f28170e134ec26dd1e999faae1739294a1a4b207
Paula Gutierrez Dias <gutierrez.012900@gmail.com>
2023-09-20 18:51:38 -0500

    Creación servicios y DTO

--- src/main/java/co/edu/uniquindio/clinica/dto/MedicoDTO.java
+++ src/main/java/co/edu/uniquindio/clinica/dto/MedicoDTO.java
@@ -3,20 +3,12 @@ package co.edu.uniquindio.clinica.dto;
 import java.util.List;
 
 public record MedicoDTO(
-
         String nombre,
-
         String cedula,
-
         int codigoCiudad,
         int codigoEspecialidad,
         String telefono,
         String email,
-        String contraseña,
-
+        String password,
         List<HorarioDTO> horarios
-
-) {
-
-
-}
+) {}
--- src/main/java/co/edu/uniquindio/clinica/servicios/AdministradorServicio.java
+++ src/main/java/co/edu/uniquindio/clinica/servicios/AdministradorServicio.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.clinica.servicios;
 
 import co.edu.uniquindio.clinica.dto.*;
+import co.edu.uniquindio.clinica.modelo.entidades.Medico;
 
 import java.util.List;
 
@@ -8,12 +9,11 @@ public interface AdministradorServicio {
 
     String crearMedico(MedicoDTO medico) throws Exception; // Deben ir como parametros los atributos que estan en el formulario(mockups)
 
-
     String actualizarMedico(int codigo, MedicoDTO medico) throws Exception;
 
     String eliminarMedico(int codigo) throws Exception;
 
-     List<MedicoDTOAdmin> ListarMedicos() throws Exception;
+    List<MedicoDTOAdmin> ListarMedicos() throws Exception;
 
     InfoMedicoDTO obtenerMedico(int codigo) throws Exception;
 
@@ -24,12 +24,4 @@ public interface AdministradorServicio {
     InfoPQRSDTO verDetallePQRS(int codigo) throws Exception;
 
     List<CitaDTOAdmin> listarCitas() throws Exception;
-
-
-
-
-
-
-
-
 }
