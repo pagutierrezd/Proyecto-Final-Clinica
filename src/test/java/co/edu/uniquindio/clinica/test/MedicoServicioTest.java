6b6db9c283cc3ebf7cad9df2a274a3341cca5554
Marcela <jmtellezr@uqvirtual.edu.co>
2023-10-19 23:52:22 -0500

    Creación de servicios del médico

--- src/test/java/co/edu/uniquindio/clinica/test/MedicoServicioTest.java
+++ src/test/java/co/edu/uniquindio/clinica/test/MedicoServicioTest.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.clinica.test;
 
 import co.edu.uniquindio.clinica.dto.admin.ItemCitaAdminDTO;
+import co.edu.uniquindio.clinica.dto.medico.RegistroAtencionDTO;
 import co.edu.uniquindio.clinica.modelo.enums.EstadoCita;
 import co.edu.uniquindio.clinica.servicios.interfaces.MedicoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,19 +19,18 @@ import java.util.List;
 public class MedicoServicioTest {
 
 
-        @Autowired
-        private MedicoServicio medicoServicio;
+    @Autowired
+    private MedicoServicio medicoServicio;
 
-        @Test
-        public void testListarCitasPendientes() throws Exception {
+    @Test
+    public void testListarCitasPendientes() throws Exception {
 
-                // Llama al método de servicio y verifica los resultados
-                List<ItemCitaAdminDTO> citasPendientes = medicoServicio.listarCitasPendientes(1);
+        // Llama al método de servicio y verifica los resultados
+        List<ItemCitaAdminDTO> citasPendientes = medicoServicio.listarCitasPendientes(1);
 
-                Assertions.assertNotEquals(0, citasPendientes.size());
-                // Verifica que no haya citas pendientes
-        }
-
+        Assertions.assertNotEquals(0, citasPendientes.size());
+        // Verifica que no haya citas pendientes
+    }
 
 
 }
